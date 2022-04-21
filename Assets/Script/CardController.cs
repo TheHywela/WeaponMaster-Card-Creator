4dc37feb23d1c1876c983ff26c307cab42657d18
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2022-04-21 17:15:39 +0100

    edited card layout

--- Assets/Script/CardController.cs
+++ Assets/Script/CardController.cs
@@ -36,10 +36,19 @@ public class CardController : MonoBehaviour
     public Color parryColor;
     public Color moveColor;
 
+    [Header("Other Icons")]
+    public Image startingDeckIcon;
+    public Image weaponDeckIcon;
+
+    public Sprite genericImage;
+    public Sprite longswordImage;
+    public Sprite halberdImage;
+    public Sprite sabreImage;
+    public Sprite swordbuckImage;
+
     private void Start()
     {
         PrintDeck();
-
     }
 
     public void Capture(string id)
@@ -65,17 +74,50 @@ public class CardController : MonoBehaviour
     {
         for (int i = 0; i < deckToPrint.cards.Length; i++)
         {
-
             ConstructCard(deckToPrint.cards[i]);
             Capture(deckToPrint.cards[i].id);
             //Print Card to Image File
-
         }
     }
 
 
     public void ConstructCard(Card card)
     {
+
+        //Starter Card Icon
+        if (card.isStarter)
+        {
+            //make starter icon visable
+            startingDeckIcon.enabled = true;
+        } 
+        else 
+        {
+            //make starter icon invisible
+            startingDeckIcon.enabled = false;
+        }
+
+        //weapon type icon
+        switch(card.weaponType){
+            case WEAPON_TYPE.GENERIC:
+                weaponDeckIcon.sprite = genericImage;
+                break;
+
+            case WEAPON_TYPE.SWORDBUCKLER:
+                weaponDeckIcon.sprite = swordbuckImage;                
+                break;
+
+            case WEAPON_TYPE.LONGSWORD:
+                weaponDeckIcon.sprite = longswordImage;                
+                break;
+            case WEAPON_TYPE.HALBERD:
+                weaponDeckIcon.sprite = halberdImage;                
+                break;
+
+            case WEAPON_TYPE.SABRE:
+                weaponDeckIcon.sprite = sabreImage;                
+                break;
+        }
+
         //Top Element
         //Card Header
         switch (card.topAction.actionType)
--- Assets/Script/ScriptableObject/Card.cs
+++ Assets/Script/ScriptableObject/Card.cs
@@ -13,6 +13,7 @@ public enum WEAPON_TYPE
 [CreateAssetMenu]
 public class Card : ScriptableObject
 {
+    public bool isStarter;
     public string id;
     public WEAPON_TYPE weaponType;
     public Action topAction;
